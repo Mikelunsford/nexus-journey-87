d0844d11abfb1b35db7d2be3d25fc65ade8b4e89
Mikelunsford <Mike@team-01.com>
2025-09-22 12:42:43 -0500

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const router = express.Router();
+import { Router, Request, Response } from 'express';
+
+const router = Router();
 
 // Example route: Home page
-router.get('/', (req, res) => {
+router.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Nexus Journey!');
 });
 
 // Example route: Docs page
-router.get('/docs', (req, res) => {
+router.get('/docs', (req: Request, res: Response) => {
   res.send('Documentation coming soon!');
 });
 
-module.exports = router;
+export default router;
